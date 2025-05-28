9d3589ff049e577d560000e93de6e8b1dd89ac85
Amy Huang <amyxjhuang@berkeley.edu>
2025-05-28 07:37:51 -0700

    Create script to download the face models

---
+++ download_faceapi_models.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Download face-api.js models for use in the Vue app
+set -e
+MODEL_DIR="web/public/models"
+BASE_URL="https://github.com/justadudewhohacks/face-api.js-models/raw/master/weights"
+
+mkdir -p "$MODEL_DIR"
+cd "$MODEL_DIR"
+
+# List of required models
+MODELS=(
+  "tiny_face_detector_model-weights_manifest.json"
+  "tiny_face_detector_model-shard1"
+  "face_landmark_68_model-weights_manifest.json"
+  "face_landmark_68_model-shard1"
+  "face_recognition_model-weights_manifest.json"
+  "face_recognition_model-shard1"
+)
+
+for FILE in "${MODELS[@]}"; do
+  if [ ! -f "$FILE" ]; then
+    echo "Downloading $FILE..."
+    curl -L -O "$BASE_URL/$FILE"
+  else
+    echo "$FILE already exists, skipping."
+  fi
+done
+
+echo "All required face-api.js models are downloaded in $MODEL_DIR." 
